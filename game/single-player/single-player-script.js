d9663552f7ad0e057cd1338f81624c813746a238
Adam Forgac <aforg@email.cz>
2023-04-09 13:41:37 +0200

    Create unbeatable AI
    The AI that can't be beaten is now working. It was created
    using different
    various options that depend on how the player moves.
    
    Most of the time, the bot just tries to defend, but when it gets
    the chance, it wins. All these functions together create an unbeatable
    AI.

---
+++ game/single-player/single-player-script.js
@@ -0,0 +1,317 @@
+// display module
+// display module
+// display module
+let playerOnePasive = false;
+
+const displayUpdate = (() => {
+    const manipuleForm = () => {
+        const nameForm = document.querySelector(".name-form");
+        nameForm.classList.add("appear");
+
+        const submitButton = document.querySelector(".submit-button");
+        submitButton.addEventListener("click", (event) => {
+            event.preventDefault();
+            const playerOneInput = document.querySelector(".player1-name input");
+            const playerTwoInput = document.querySelector(".player2-name input");
+            if((playerOneInput.checkValidity() === false || playerOneInput.reportValidity() === false) || (playerTwoInput.checkValidity() === false || playerTwoInput.reportValidity() === false)) {
+                false
+            } else {
+                nameForm.classList.remove("appear");
+                const nameOneValue = playerOneInput.value;
+                const nameTwoValue = playerTwoInput.value;
+                document.querySelector(".profile1 p").textContent = nameOneValue;
+                document.querySelector(".profile2 p").textContent = nameTwoValue;
+                document.querySelector(".win-num1").textContent = 0;
+                document.querySelector(".win-num2").textContent = 0;
+                const playerOne = Players("X", "active");
+                const bot = Players("O", "pasive");
+                playerOne.playGame(playerOne, bot);
+                restartStats();
+            }
+        })
+    };
+
+    const playerOneWins = () => {
+        document.querySelector("body").style.overflow = "hidden";
+        document.querySelector(".win-name").textContent = document.querySelector(".profile1 p").textContent;
+        document.querySelector(".mark").textContent = "X";
+        document.querySelector(".winning-banner").classList.add("active");
+
+        // ADDS POINT TO PLAYER NUMBER 1
+
+        const numStat = Number(document.querySelector(".win-num1").textContent);
+        const numStatTotal = numStat + 1;
+        document.querySelector(".win-num1").textContent = numStatTotal;
+
+        // ADDS ROUND NUMBER
+
+        const numStat2 = Number(document.querySelector(".score-number").textContent);
+        const numStatTotal2 = numStat2 + 1;
+        document.querySelector(".score-number").textContent = numStatTotal2;
+
+        // ADDS POINT TO SCORE
+
+        const numStat3 = Number(document.querySelector(".number1").textContent);
+        const numStatTotal3 = numStat3 + 1;
+        document.querySelector(".number1").textContent = numStatTotal3;
+
+        playerOnePasive = false;
+        nextRound();
+    }
+
+    const playerTwoWins = () => {
+        document.querySelector("body").style.overflow = "hidden";
+        document.querySelector(".win-name").textContent = document.querySelector(".profile2 p").textContent;
+        document.querySelector(".mark").textContent = "O";
+        document.querySelector(".winning-banner").classList.add("active");
+
+        // ADDS POINT TO PLAYER NUMBER 2
+
+        const numStat4 = Number(document.querySelector(".win-num2").textContent);
+        const numStatTotal4 = numStat4 + 1;
+        document.querySelector(".win-num2").textContent = numStatTotal4;
+            
+        // ADDS ROUND NUMBER
+
+        const numStat6 = Number(document.querySelector(".score-number").textContent);
+        const numStatTotal6 = numStat6 + 1;
+        document.querySelector(".score-number").textContent = numStatTotal6;
+            
+        // ADDS POINT TO SCORE
+
+        const numStat5 = Number(document.querySelector(".number2").textContent);
+        const numStatTotal5 = numStat5 + 1;
+        document.querySelector(".number2").textContent = numStatTotal5;
+
+        playerOnePasive = false;
+        nextRound();
+    }
+
+    const nextRound = () => {
+        document.querySelector(".next-round button").addEventListener("click", (event) => {
+            document.querySelector("body").style.overflow = "visible";
+            console.log("ůalkdfjůaslkdf")
+            document.querySelector(".winning-banner").classList.remove("active");
+
+            // CLEARS THE GAMEBOARD
+
+            gameBoard.cleanPlan();
+            event.stopImmediatePropagation();
+        })
+    }
+
+    const restartStats = () => {
+        document.querySelector(".number1").textContent = 0;
+        document.querySelector(".number2").textContent = 0;
+        document.querySelector(".score-number").textContent = 1;
+    }
+
+    return {manipuleForm, playerOneWins, playerTwoWins};
+})()
+
+displayUpdate.manipuleForm();
+
+
+
+// gameBoard module
+// gameBoard module
+// gameBoard module
+
+
+
+const gameBoard = (() => {
+    let gamePlan = ["", "", "", "", "", "", "", "", ""];
+    const getCell = (num, mark) => {
+        if(gamePlan[num] === "X" || gamePlan[num] === "O") {
+            false;
+        } else {
+            gamePlan[num] = mark;
+
+            for(let i = 0; i <= gamePlan.length-1; i++) {
+                const playGround = document.querySelector(".playground");
+                playGround.querySelector(`[data-attribute="${i}"]`).textContent = gamePlan[i];
+            }
+        }
+    }
+
+    const cleanPlan = () => {
+        for(let i = 0; i <= gamePlan.length-1; i++) {
+                const allCells = document.querySelectorAll(".cell");
+
+                allCells.forEach(cell => {
+                    cell.textContent = "";
+                })
+
+                gamePlan[i] = "";
+            }
+        }
+    
+
+    const checkForWin = () => {
+        if(
+            (gamePlan[0] === "X" && gamePlan[1] === "X" && gamePlan[2] === "X") ||
+            (gamePlan[3] === "X" && gamePlan[4] === "X" && gamePlan[5] === "X") ||
+            (gamePlan[6] === "X" && gamePlan[7] === "X" && gamePlan[8] === "X") ||
+            (gamePlan[0] === "X" && gamePlan[3] === "X" && gamePlan[6] === "X") ||
+            (gamePlan[1] === "X" && gamePlan[4] === "X" && gamePlan[7] === "X") ||
+            (gamePlan[2] === "X" && gamePlan[5] === "X" && gamePlan[8] === "X") ||
+            (gamePlan[0] === "X" && gamePlan[4] === "X" && gamePlan[8] === "X") ||
+            (gamePlan[2] === "X" && gamePlan[4] === "X" && gamePlan[6] === "X")
+        ) {
+            setTimeout(function() { displayUpdate.playerOneWins(); }, 100);
+        } else if(
+            (gamePlan[0] === "O" && gamePlan[1] === "O" && gamePlan[2] === "O") ||
+            (gamePlan[3] === "O" && gamePlan[4] === "O" && gamePlan[5] === "O") ||
+            (gamePlan[6] === "O" && gamePlan[7] === "O" && gamePlan[8] === "O") ||
+            (gamePlan[0] === "O" && gamePlan[3] === "O" && gamePlan[6] === "O") ||
+            (gamePlan[1] === "O" && gamePlan[4] === "O" && gamePlan[7] === "O") ||
+            (gamePlan[2] === "O" && gamePlan[5] === "O" && gamePlan[8] === "O") ||
+            (gamePlan[0] === "O" && gamePlan[4] === "O" && gamePlan[8] === "O") ||
+            (gamePlan[2] === "O" && gamePlan[4] === "O" && gamePlan[6] === "O")
+        ) {
+            setTimeout(function() { displayUpdate.playerTwoWins(); }, 100);
+        }
+    }
+    return {getCell, gamePlan, checkForWin, cleanPlan};
+})()
+
+
+// player module
+
+
+
+
+const Players = (mark, activity) => {
+    const getMark = () => mark;
+    const getActivity = () => activity;
+    const playGame = (player1, bot) => {
+        const allCells = document.querySelectorAll(".cell");
+
+        allCells.forEach(cell => {
+            cell.addEventListener("click", (event) => {
+                if(playerOnePasive === false)  {
+                    const cellNumber = Number(cell.getAttribute("data-attribute"));
+                    if(gameBoard.gamePlan[cellNumber] !== "X" && gameBoard.gamePlan[cellNumber] !== "O") {
+                        gameBoard.getCell(cellNumber, "X");
+                        playerOnePasive = true;
+                        gameBoard.checkForWin();
+                        playBot();
+                    }  else if(gameBoard.gamePlan[cellNumber] === "X" && gameBoard.gamePlan[cellNumber] === "O") {
+                        false;
+                    }
+                }
+                })
+    
+            })  
+
+
+        function playBot() {
+            let currectCell;
+
+            let count = {};
+            gameBoard.gamePlan.forEach(function(i) { count[i] = (count[i]||0) + 1;});
+
+            if((gameBoard.gamePlan[4] !== "X" && count.X === 1) && (gameBoard.gamePlan[4] !== "O" && count.X === 1)) {
+                currectCell = 4;
+                console.log("start1");
+            } else if((gameBoard.gamePlan[4] === "X" && count.X === 1) || (gameBoard.gamePlan[4] === "O" && count.X === 1))  {
+                let randomNumber = Math.random() * 9;
+                let wholeRandomNumber = Math.floor(randomNumber);
+                console.log(wholeRandomNumber);
+                if(wholeRandomNumber === 4) {
+                    wholeRandomNumber-1;
+                }
+                currectCell = wholeRandomNumber;
+                console.log("start2");
+            } else if((((gameBoard.gamePlan[3] === "X") && (gameBoard.gamePlan[6] === "X")) || ((gameBoard.gamePlan[4] === "X") && (gameBoard.gamePlan[8] === "X")) || ((gameBoard.gamePlan[1] === "X") && (gameBoard.gamePlan[2] === "X"))) && (gameBoard.gamePlan[0] === "")) {
+                currectCell = 0;
+                console.log("0");
+            } else if((((gameBoard.gamePlan[4] === "X") && (gameBoard.gamePlan[7] === "X")) || ((gameBoard.gamePlan[0] === "X") && (gameBoard.gamePlan[2] === "X"))) && (gameBoard.gamePlan[1] === "")) {
+                currectCell = 1;
+                console.log("1");
+            } else if((((gameBoard.gamePlan[0] === "X") && (gameBoard.gamePlan[1] === "X")) || ((gameBoard.gamePlan[6] === "X") && (gameBoard.gamePlan[4] === "X")) || ((gameBoard.gamePlan[5] === "X") && (gameBoard.gamePlan[8] === "X"))) && (gameBoard.gamePlan[2] === "")) {
+                currectCell = 2;
+                console.log("2");
+            } else if((((gameBoard.gamePlan[0] === "X") && (gameBoard.gamePlan[6] === "X")) || ((gameBoard.gamePlan[4] === "X") && (gameBoard.gamePlan[5] === "X"))) && (gameBoard.gamePlan[3] === "")) {
+                currectCell = 3;
+                console.log("3");
+            } else if((((gameBoard.gamePlan[1] === "X") && (gameBoard.gamePlan[7] === "X")) || ((gameBoard.gamePlan[2] === "X") && (gameBoard.gamePlan[6] === "X")) || ((gameBoard.gamePlan[5] === "X") && (gameBoard.gamePlan[3] === "X")) || ((gameBoard.gamePlan[8] === "X") && (gameBoard.gamePlan[0] === "X"))) && (gameBoard.gamePlan[4] === "")) {
+                currectCell = 4;
+                console.log("4");
+            } else if((((gameBoard.gamePlan[2] === "X") && (gameBoard.gamePlan[8] === "X")) || ((gameBoard.gamePlan[3] === "X") && (gameBoard.gamePlan[4] === "X"))) && (gameBoard.gamePlan[5] === "")) {
+                currectCell = 5;
+                console.log("5");
+            } else if((((gameBoard.gamePlan[0] === "X") && (gameBoard.gamePlan[3] === "X")) || ((gameBoard.gamePlan[4] === "X") && (gameBoard.gamePlan[2] === "X")) || ((gameBoard.gamePlan[7] === "X") && (gameBoard.gamePlan[8] === "X"))) && (gameBoard.gamePlan[6] === "")) {
+                currectCell = 6;
+                console.log("6");
+            } else if((((gameBoard.gamePlan[1] === "X") && (gameBoard.gamePlan[4] === "X")) || ((gameBoard.gamePlan[6] === "X") && (gameBoard.gamePlan[8] === "X"))) && (gameBoard.gamePlan[7] === "")) {
+                currectCell = 7;
+                console.log("7");
+            } else if((((gameBoard.gamePlan[2] === "X") && (gameBoard.gamePlan[5] === "X")) || ((gameBoard.gamePlan[6] === "X") && (gameBoard.gamePlan[7] === "X")) || ((gameBoard.gamePlan[0] === "X") && (gameBoard.gamePlan[4] === "X"))) && (gameBoard.gamePlan[8] === "")) {
+                currectCell = 8;
+                console.log("8");
+            } else if((gameBoard.gamePlan[1] === "X" && gameBoard.gamePlan[3] === "X") && (gameBoard.gamePlan[0] === "")) {
+                currectCell = 0;
+            } else if((gameBoard.gamePlan[1] === "X" && gameBoard.gamePlan[5] === "X") && (gameBoard.gamePlan[2] === "")) {
+                currectCell = 2;
+            } else if((gameBoard.gamePlan[5] === "X" && gameBoard.gamePlan[7] === "X") && (gameBoard.gamePlan[8] === "")) {
+                currectCell = 8;
+            } else if((gameBoard.gamePlan[3] === "X" && gameBoard.gamePlan[7] === "X") && (gameBoard.gamePlan[6] === "")) {
+                currectCell = 6;
+
+            } else if(((gameBoard.gamePlan[0] === "O" && gameBoard.gamePlan[1] === "O") || (gameBoard.gamePlan[6] === "O" && gameBoard.gamePlan[4] === "O") || (gameBoard.gamePlan[5] === "O" && gameBoard.gamePlan[8] === "O")) && (gameBoard.gamePlan[2] === "")) {
+                currectCell = 2;
+                console.log("attacking2")
+            } else if(((gameBoard.gamePlan[1] === "O" && gameBoard.gamePlan[2] === "O") || (gameBoard.gamePlan[3] === "O" && gameBoard.gamePlan[6] === "O") || (gameBoard.gamePlan[4] === "O" && gameBoard.gamePlan[8] === "O")) && (gameBoard.gamePlan[0] === "")) {
+                currectCell = 0;
+                console.log("attacking0")
+            } else if(((gameBoard.gamePlan[0] === "O" && gameBoard.gamePlan[3] === "O") || (gameBoard.gamePlan[2] === "O" && gameBoard.gamePlan[4] === "O") || (gameBoard.gamePlan[7] === "O" && gameBoard.gamePlan[8] === "O")) && (gameBoard.gamePlan[6] === "")) {
+                currectCell = 6;
+                console.log("attacking6")
+            } else if(((gameBoard.gamePlan[2] === "O" && gameBoard.gamePlan[5] === "O") || (gameBoard.gamePlan[0] === "O" && gameBoard.gamePlan[4] === "O") || (gameBoard.gamePlan[6] === "O" && gameBoard.gamePlan[7] === "O")) && (gameBoard.gamePlan[8] === "")) {
+                currectCell = 8;
+                console.log("attacking8")
+            } else if(((gameBoard.gamePlan[0] === "0" && gameBoard.gamePlan[2]) || (gameBoard.gamePlan[4] === "0" && gameBoard.gamePlan[7])) && (gameBoard.gamePlan[1] === "")) {
+                currectCell = 1;
+                console.log("attacking1")
+            } else if(((gameBoard.gamePlan[2] === "0" && gameBoard.gamePlan[8]) || (gameBoard.gamePlan[3] === "0" && gameBoard.gamePlan[4])) && (gameBoard.gamePlan[5] === "")) {
+                currectCell = 5;
+                console.log("attacking5")
+            } else if(((gameBoard.gamePlan[1] === "0" && gameBoard.gamePlan[4]) || (gameBoard.gamePlan[6] === "0" && gameBoard.gamePlan[8])) && (gameBoard.gamePlan[7] === "")) {
+                currectCell = 7;
+                console.log("attacking7")
+            } else if(((gameBoard.gamePlan[0] === "0" && gameBoard.gamePlan[6]) || (gameBoard.gamePlan[4] === "0" && gameBoard.gamePlan[5])) && (gameBoard.gamePlan[3] === "")) {
+                currectCell = 3;
+                console.log("attacking3")
+            } else if(((gameBoard.gamePlan[1] === "0" && gameBoard.gamePlan[7]) || (gameBoard.gamePlan[3] === "0" && gameBoard.gamePlan[5]) || (gameBoard.gamePlan[2] === "0" && gameBoard.gamePlan[6]) || (gameBoard.gamePlan[0] === "0" && gameBoard.gamePlan[8])) && (gameBoard.gamePlan[4] === "")) {
+                currectCell = 4;
+                console.log("attacking4")
+            } else {
+                const index = gameBoard.gamePlan.indexOf("O"); 
+                currectCell = index+1;
+                console.log("+1")
+                if(gameBoard.gamePlan[currectCell] < 0 || gameBoard.gamePlan[currectCell] > 8 || gameBoard.gamePlan[currectCell] !== "") {
+                    const random = gameBoard.gamePlan.indexOf("");
+                    currectCell = random;
+                }  
+            }
+          
+
+    
+
+            if(playerOnePasive) {
+                if(gameBoard.gamePlan[currectCell] !== "X" && gameBoard.gamePlan[currectCell] !== "O") {
+                    gameBoard.getCell(currectCell, "O");
+                    playerOnePasive = false;
+                    gameBoard.checkForWin();
+                } else if(gameBoard.gamePlan[currectCell] === "X" && gameBoard.gamePlan[currectCell] === "O") {
+                    false;
+                }
+            }
+        }
+
+    }
+
+    return {playGame, getMark, getActivity, playerOnePasive};
+}
+
